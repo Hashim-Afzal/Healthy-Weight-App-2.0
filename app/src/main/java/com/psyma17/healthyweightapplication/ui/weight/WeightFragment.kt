28d3beae0c1f65330e343e970f67be7bdb9c9334
psyma17 <psyma17@exmail.nottingham.ac.uk>
2022-03-21 21:19:38 +0000

    Adding weight implementation

--- app/src/main/java/com/psyma17/healthyweightapplication/ui/weight/WeightFragment.kt
+++ app/src/main/java/com/psyma17/healthyweightapplication/ui/weight/WeightFragment.kt
@@ -5,15 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import com.psyma17.healthyweightapplication.data.WeightData
 import com.psyma17.healthyweightapplication.databinding.FragmentWeightBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.lang.StringBuilder
 
 class WeightFragment : Fragment() {
 
     private lateinit var weightViewModel: WeightViewModel
     private var _binding: FragmentWeightBinding? = null
+    private val weightDataCollectionRef = Firebase.firestore.collection("weight")
+    lateinit var auth: FirebaseAuth
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -34,9 +49,76 @@ class WeightFragment : Fragment() {
         weightViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        auth = FirebaseAuth.getInstance()
+        subscribeToRealTimeUpdates()
+        binding.btnWeightSubmit.setOnClickListener {
+            btnSaveWeightData()
+        }
+        binding.btnWeightRemoveLast.setOnClickListener {
+            btnRemoveLastWeightData()
+        }
+
         return root
     }
 
+    private fun subscribeToRealTimeUpdates() {
+        weightDataCollectionRef.addSnapshotListener { value, firebaseFirestoreException ->
+            firebaseFirestoreException?.let {
+                Toast.makeText(context, it.message, Toast.LENGTH_LONG).show()
+                return@addSnapshotListener
+            }
+            value?.let {
+                val stringBuilder = StringBuilder()
+                for (document in it.documents) {
+                    val weightData = document.toObject<WeightData>()
+                    stringBuilder.append("$weightData\n")
+                }
+                binding.textViewWeight.text = stringBuilder.toString()
+            }
+        }
+    }
+
+    private fun saveWeightData(weightData: WeightData) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            weightDataCollectionRef.add(weightData).await()
+            withContext(Dispatchers.Main) {
+                Toast.makeText(context, "Successfully saved data.", Toast.LENGTH_LONG).show()
+            }
+        } catch(e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(context, e.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun btnSaveWeightData() {
+        val temp = binding.editTextNumberDecimal.text.toString()
+        if (temp == "") {
+            return
+        }
+        val currentWeight = temp.toDouble()
+        if (currentWeight < 10 || currentWeight > 300) {
+            return
+        }
+        val currentTime = System.currentTimeMillis()
+        val weightData = WeightData(currentWeight, currentTime)
+        saveWeightData(weightData)
+    }
+
+    private fun btnRemoveLastWeightData() = CoroutineScope(Dispatchers.IO).launch {
+        val querySnapshot = weightDataCollectionRef.orderBy("time", Query.Direction.DESCENDING).limit(1).get().await()
+
+        if (querySnapshot.documents.isNotEmpty()) {
+            try {
+                weightDataCollectionRef.document(querySnapshot.documents[0].id).delete().await()
+            } catch (e: java.lang.Exception) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(context, e.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
